4b9330ba585adc1a3d61bd9d81571ad8d2753444
Jorge Alirio Mendoza Rincon <aliriomri@gmail.com>
2026-04-02 20:59:51 -0500

    configuracion firebase

--- realtime.js
+++ realtime.js
@@ -1,16 +1,20 @@
 import { getState } from "./tools.js";
 
 const firebaseConfig = {
-  apiKey: "TU_APIKEY",
-  authDomain: "TU_AUTH",
-  databaseURL: "TU_DATABASE_URL",
-  projectId: "TU_ID"
+  apiKey: "AIzaSyA3Eo8CTL0CqZ-_qrVJmloL2YpYGyO2Q_Q",
+  authDomain: "mathboardpro-3f07b.firebaseapp.com",
+  databaseURL: "https://mathboardpro-3f07b-default-rtdb.firebaseio.com",
+  projectId: "mathboardpro-3f07b",
+  storageBucket: "mathboardpro-3f07b.firebasestorage.app",
+  messagingSenderId: "318885521756",
+  appId: "1:318885521756:web:9995c0ddccea4b6080f999",
+  measurementId: "G-R7K6V2LMEL"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
-const board = db.ref("mathboard");
+const board = db.ref("mathboardpro");
 
 export function enviarTrazo(trazo){
     board.push(trazo);
